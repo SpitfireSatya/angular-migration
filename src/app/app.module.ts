b5eb211ad790978a7307903165cb015a086ac8a2
Spitfire <twisterintwist@gmail.com>
2017-06-08 21:13:23 +0530

    Splitting up folder structure to manage ng and ngJS code base. Moving content from app.module to main.module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { MainModule } from './ng/main.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MainModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ng/main.module.ts
@@ -0,0 +1,14 @@
+
+import { NgModule } from '@angular/core';
+
+import { MainComponent } from './main.component';
+
+@NgModule({
+  declarations: [
+    MainComponent
+  ],
+  imports: [],
+  providers: [],
+  exports: [MainComponent]
+})
+export class MainModule { }
